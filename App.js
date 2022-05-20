24a5004d4bb9ba61e3a92c795294cf12d11794db
Bui Duc Thanh <e0817820@u.nus.edu>
2022-05-20 16:07:15 +0800

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Slider} from '@miblanchard/react-native-slider';
+import React from "react";
 
 export default function App() {
+  let name = "Thanh";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{"Hello World, " + name}</Text>
     </View>
   );
 }
@@ -13,8 +15,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#afc',
+    backgroundColor: '#123456',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+// const CallerComponent = () => {
+//   return (
+//   <View>
+//     <CalleeComponent phrase="Hello World!" />
+//   </View>
+//   )
+//  };
+//  class CalleeComponent extends React.Component {
+//   render() {
+//   const { phrase } = this.props;
+//   return (
+//   <Text>{phrase}</Text>
+//   );
+//   };
+//  };
+ 
+//  const CalleeComponent2 = (pr) =>  {
+//   return (
+//     <Text>{pr.phrase}</Text>
+//   );
+// };
+
+// export default CallerComponent;
