7c60ba4ddf2332eed5437068bef37b5cbc67ccac
Bui Duc Thanh <e0817820@u.nus.edu>
2022-06-14 22:16:49 +0800

    Nav bar v3

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -6,20 +6,61 @@ import Input from '../screens/Input';
 import Stats from '../screens/Stats';
 import Other from '../screens/Other';
 import { colors } from '../components/colors';
-import { Ionicons, FontAwesome } from '@expo/vector-icons';
+import { Ionicons, Entypo } from '@expo/vector-icons';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Animation } from 'react-native-animatable';
 const Tab = createBottomTabNavigator();
 const { beige, darkBlue, lightBlue } = colors;
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: lightBlue,
+    shadowOffset: {
+      width:0,
+      height:8,
+    },
+    shadowRadius:3.5,
+    elevation:5
+  }
+})
+
+const CustomInputButton = ({children, onPress}) => {
+  return (
+    <TouchableOpacity 
+      onPress={onPress}
+      style={{
+        top:-20,
+        justifyContent:'center',
+        alignItems:'center',
+        ...styles.shadow,
+      }}>
+        <View style={{
+          width:66,
+          height:66,
+          borderRadius:33,
+          alignItems:'center',
+          justifyContent:'center',
+          backgroundColor:lightBlue,
+        }}>
+          <Entypo name="plus" size ={36} color={beige}/>
+        </View>
+    </TouchableOpacity>
+  )
+}
 
 const Tabs = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} 
+      <Tab.Screen name="Home" component={Home}  
         options={{
           tabBarShowLabel:false,
           headerShown:false,
           tabBarLabel: "Home",
           tabBarIcon: ({focused}) => (
-            <Ionicons name={focused ? 'home' : 'home-outline'} size ={28} color={focused ? darkBlue : lightBlue}/>
+            <View style={{alignItems:'center', justifyContent:'center', top:8}}>
+              <Ionicons name={focused ? 'home' : 'home-outline'} size ={28} color={focused ? darkBlue : lightBlue}/>
+              <Text style={{fontSize:11, fontWeight:'600', color:focused ? darkBlue : lightBlue}}>Home</Text>
+            </View>
           )
         }}/>
       <Tab.Screen name="Calendar" component={Calendar} 
@@ -28,7 +69,10 @@ const Tabs = () => {
           headerShown:false,
           tabBarLabel: "Calendar",
           tabBarIcon: ({focused}) => (
-            <Ionicons name={focused ? "calendar" : "calendar-outline"} size ={28} color={focused ? darkBlue : lightBlue}/>
+            <View style={{alignItems:'center', justifyContent:'center', top:8}}>
+              <Ionicons name={focused ? "calendar" : "calendar-outline"} size ={28} color={focused ? darkBlue : lightBlue}/>
+              <Text style={{fontSize:11, fontWeight:'600', color:focused ? darkBlue : lightBlue}}>Calendar</Text>
+            </View>
           )
         }}/>
       <Tab.Screen name="Input" component={Input} 
@@ -37,7 +81,20 @@ const Tabs = () => {
           headerShown:false,
           tabBarLabel: "Input",
           tabBarIcon: ({focused}) => (
-            <FontAwesome name={focused ? "plus-square" : "plus-square-o"} size ={28} color={focused ? darkBlue : lightBlue}/>
+            <View style={{
+              top:-20,
+              width:66,
+              height:66,
+              borderRadius:33,
+              alignItems:'center',
+              justifyContent:'center',
+              backgroundColor: focused ? darkBlue : lightBlue,
+            }}>
+              <Entypo name="plus" size ={36} color={beige}/>
+            </View>
+          ),
+          tabBarButton: props => (
+            <TouchableOpacity {...props} />
           )
         }}/>
       <Tab.Screen name="Stats" component={Stats} 
@@ -46,7 +103,10 @@ const Tabs = () => {
           headerShown:false,
           tabBarLabel: "Stats",
           tabBarIcon: ({focused}) => (
-            <Ionicons name={focused ? "bar-chart" : "bar-chart-outline"} size ={28} color={focused ? darkBlue : lightBlue}/>
+            <View style={{alignItems:'center', justifyContent:'center', top:8}}>
+              <Ionicons name={focused ? "bar-chart" : "bar-chart-outline"} size ={28} color={focused ? darkBlue : lightBlue}/>
+              <Text style={{fontSize:11, fontWeight:'600', color:focused ? darkBlue : lightBlue}}>Stats</Text>
+            </View>
           ),
           
         }}/>
@@ -56,7 +116,10 @@ const Tabs = () => {
           headerShown:false,
           tabBarLabel: "Other",
           tabBarIcon: ({focused}) => (
-            <Ionicons name={focused ? "ellipsis-horizontal-sharp" : "ellipsis-horizontal-outline"} size={28} color={focused ? darkBlue : lightBlue} />
+            <View style={{alignItems:'center', justifyContent:'center', top:8}}>
+              <Ionicons name={focused ? "ellipsis-horizontal-sharp" : "ellipsis-horizontal-outline"} size={28} color={focused ? darkBlue : lightBlue} />
+              <Text style={{fontSize:11, fontWeight:'600', color:focused ? darkBlue : lightBlue}}>Other</Text>
+            </View>
           )
         }}/>
     </Tab.Navigator>
