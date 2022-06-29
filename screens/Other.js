3ae889f2588cbf8b06a7f14357859742d6880544
Bui Duc Thanh <bdthanh200403@gmail.com>
2022-06-29 14:40:32 +0800

    add user's image and username on Other screen

--- screens/Other.js
+++ screens/Other.js
@@ -1,25 +1,112 @@
-import React, {useEffect} from 'react';
-import { View, Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-web';
-import styles from '../components/styles';
+import React, {useEffect, useState} from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity, Image, Platform, Alert } from 'react-native';
 import PressableText from '../components/Containers/PressableText';
 import { handleSignOut } from '../api/authentication';
-
+import { StatusBarHeight } from '../components/constants';
+import * as ImagePicker from 'expo-image-picker';
+import { colors } from '../components/colors';
+import { Avatar, Drawer } from 'react-native-paper';
+const {beige, brown, darkBlue, lightBlue, darkYellow, lightYellow} = colors;
 const Other = ({navigation}) => {
+  const [image, setImage] = useState('');
+  const [username, setUsername] = useState('Team Grape')
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+  const askChangeAvatar = () => {
+    Alert.alert("Change avatar?", "", [
+      {text: 'No', onPress: () => console.log('Alert closed')},
+      {text: 'Yes', onPress: pickImage}
+    ]);
+  }
+
   return (
-    <View style={styles.mainContainer}>
-      <PressableText onPress={() => {
-        handleSignOut(navigation)
-      }}>
-        Sign Out
-      </PressableText>
-      {/* <PressableText onPress={() => {
-        displayUserID()
-      }}>
-        getUserID
-      </PressableText> */}
-    </View>
+    <>
+      <View style={styles.header}>
+        <Text style={styles.boldBlueHeaderText}>Other</Text>
+      </View>
+      <ScrollView>
+        <View style={styles.userView}>
+          <View style={styles.imageView}>
+            <TouchableOpacity onPress={askChangeAvatar}>
+            {image == '' && <Avatar.Image source={{ uri: "https://cdn.landesa.org/wp-content/uploads/default-user-image.png" }} size={80} />}
+            {image != '' && <Avatar.Image source={{ uri: image }} size={80} />}
+            </TouchableOpacity>
+          </View>
+          <View style={styles.usernameView}>
+            <Text style={styles.boldBlackHeaderText}>{username}</Text>
+          </View>
+        </View>
+        <PressableText onPress={() => {
+          handleSignOut(navigation)
+        }}>
+          Sign Out
+        </PressableText>
+        {/* <PressableText onPress={() => {
+          displayUserID()
+        }}>
+          getUserID
+        </PressableText> */}
+      </ScrollView>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 25,
+    paddingTop: StatusBarHeight + 30,
+    backgroundColor: beige,
+  }, 
+  boldBlueHeaderText: {
+    fontSize: 34,
+    color: darkBlue,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+  boldBlackHeaderText: {
+    fontSize: 25,
+    fontWeight: '500',
+    color: '#000',
+    marginBottom: 10,
+  },
+  header: {
+    alignItems:'center', 
+    justifyContent:'flex-end',
+    backgroundColor:lightYellow,
+    borderBottomColor:'#808080',
+    borderBottomWidth:1,
+    paddingTop:3,
+    height: StatusBarHeight + 48,
+  },
+  userView: {
+    flexDirection: 'row',
+    height:120,
+    paddingLeft:20
+  },
+  imageView: {
+    flex: 3,
+    justifyContent:'center'
+  },
+  usernameView: {
+    flex: 7,
+    paddingLeft:0,
+    alignItems:'flex-start',
+    justifyContent:'center',
+  }
+})
 
 export default Other;
